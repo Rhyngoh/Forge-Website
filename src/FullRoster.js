314d89d50fab8f70668e71c9760a29e3b7251f85
rhyngoh <ngoh.ryan@gmail.com>
2018-08-27 22:15:57 -0500

    added some description for basic pieces

--- src/FullRoster.js
+++ src/FullRoster.js
@@ -6,6 +6,8 @@ import { withStyles } from '@material-ui/core/styles';
 import jss from 'jss';
 import preset from 'jss-preset-default';
 import PropTypes from 'prop-types';
+import exampleNumber from './images/Example-Numbered.png';
+import exampleNonNumber from './images/Example-Non-Numbered.png';
 
 jss.setup(preset());
 
@@ -14,7 +16,7 @@ const styles = {
 		flexGrow: 1,
 		width: '100%',
 		margin: 0,
-		'& h2': {
+		'& h1': {
 			textAlign: 'center'
 		}
 	},
@@ -26,6 +28,12 @@ const styles = {
 		'&:nth-child(odd)': {
 			flexDirection: 'row-reverse'
 		}
+	},
+	pieceInfo: {
+		padding: '2rem'
+	},
+	exampleBoard: {
+		padding: '1rem'
 	}
 }
 class FullRoster extends React.Component {
@@ -34,17 +42,31 @@ class FullRoster extends React.Component {
 		return(
 			<div>
 				<Grid className={classes.root}>
-					<h2>Piece Information</h2>
+					<h1>Piece Information</h1>
+					<Divider />
+					<Grid container spacing={24} className={classes.pieceInfo} alignItems='center' justify='center'>
+						<Grid xs={8} item>
+							<h2>Basic Pieces</h2>
+							<p>
+								Each piece on the board has an icon or pattern that will differentiate itself from the other types of pieces. The basic pieces consists of <b>numbered</b> pieces (1, 2, 3, 4) and <b>non-numbered</b> chess pieces.
+							</p>
+							<p>
+								Numbered pieces require the next move to be a certain number of pieces away from the piece struck, going in all 4 cardinal directions and 4 ordinal directions.
+							</p>
+							<img src={exampleNumber} className={classes.exampleBoard} width='200' alt='Example Numbered Board'/>
+							<img src={exampleNonNumber} className={classes.exampleBoard} width='200' alt='Example Non-Numbered Board'/>
+						</Grid>
+					</Grid>
 					<Divider />
 					{
-						PieceAPI.all().map(p => (
+						PieceAPI.basic().map(p => (
 							<Grid container className={classes.pieceContainer} alignItems='center' justify='center' spacing={24} key={p.number}>
 								<Grid xs={12} sm={4} item>
 									<h3>{p.name}</h3>
 									<p>{p.description}</p>
 								</Grid>
 								<Grid xs={12} sm={4} item>
-									<img src={p.image} width='200' alt={p.name}/>
+									<img src={p.image} width='150' alt={p.name}/>
 								</Grid>
 							</Grid>
 						))
--- src/PieceAPI.js
+++ src/PieceAPI.js
@@ -1,30 +1,44 @@
 const PieceAPI = {
-	pieces: [
-		{ number: 1, name: 'Basic Piece 1', link: 'Basic-Piece-1', description: 'sample description1', image: '/images/Basic-1.png' },
-		{ number: 2, name: 'Basic Piece 2', link: 'Basic-Piece-2', description: 'sample description2', image: '/images/Basic-2.png' },
-		{ number: 3, name: 'Basic Piece 3', link: 'Basic-Piece-3', description: 'sample description3', image: '/images/Basic-3.png' },
-		{ number: 4, name: 'Basic Piece 4', link: 'Basic-Piece-4', description: 'sample description4', image: '/images/Basic-4.png' },
-		{ number: 5, name: 'Knight', link: 'Knight', description: 'sample description', image: '/images/Knight.png' },
-		{ number: 6, name: 'Rook', link: 'Rook', description: 'sample description', image: '/images/Rook.png' },
-		{ number: 7, name: 'Bishop', link: 'Bishop', description: 'sample description', image: '/images/Bishop.png' },
-		{ number: 8, name: 'Queen', link: 'Queen', description: 'sample description', image: '/images/Queen.png' },
-	],
-	reversePieces: [
-		{ number: 9, name: 'Reverse Rook', link: 'Reverse-Rook', description: 'sample description', image: '/images/Reverse-Rook.png' },
-		{ number: 10, name: 'Reverse Bishop', link: 'Reverse-Bishop', description: 'sample description', image: '/images/Reverse-Bishop.png' },
-		{ number: 11, name: 'Reverse Queen', link: 'Reverse-Queen', description: 'sample description', image: '/images/Reverse-Queen.png' },
-	],
-	superPieces: [
-		{ number: 12, name: 'Super Rook', link: 'Super-Rook', description: 'sample description', image: '/images/Super-Rook.png' },
-		{ number: 13, name: 'Super Bishop', link: 'Super-Bishop', description: 'sample description', image: '/images/Super-Bishop.png' },
-		{ number: 14, name: 'Super Queen', link: 'Super-Queen', description: 'sample description', image: '/images/Super-Queen.png' },
-	],
+	pieces: {
+		basicPieces: [
+			{ number: 1, name: 'Basic Piece 1', link: 'Basic-Piece-1', description: 'Strike this piece and hit any surrounding tile. This is a wonderful tile to continue combinations.', image: '/images/Basic-1.png' },
+			{ number: 2, name: 'Basic Piece 2', link: 'Basic-Piece-2', description: 'This piece allows the forge master to hit another tile 2 spaces away.', image: '/images/Basic-2.png' },
+			{ number: 3, name: 'Basic Piece 3', link: 'Basic-Piece-3', description: 'Larger numbered pieces have limited number of tiles to strike next turn. Use them wisely!', image: '/images/Basic-3.png' },
+			{ number: 4, name: 'Basic Piece 4', link: 'Basic-Piece-4', description: 'The largest numbered piece extends 4 spaces.', image: '/images/Basic-4.png' },
+			{ number: 5, name: 'Knight', link: 'Knight', description: 'The knight must be followed by striking a piece two squares in a cardinal direction, and one square perpendicular to that direction.', image: '/images/Knight.png' },
+			{ number: 6, name: 'Rook', link: 'Rook', description: 'The Rook must be followed by striking a piece at the edge of the board and in line with the Rook horizontally or vertically (cardinal directions)', image: '/images/Rook.png' },
+			{ number: 7, name: 'Bishop', link: 'Bishop', description: 'The Bishop moves in any ordinal direction (diagonal) and must be followed by striking a piece at the edge of the board.', image: '/images/Bishop.png' },
+			{ number: 8, name: 'Queen', link: 'Queen', description: 'The Queen has great flexibility and may be followed by striking a piece at the edge of the board and horizontally, vertically, or diagonally in line with the Queen.', image: '/images/Queen.png' },
+			{ number: 9, name: 'Joker', link: 'Joker', description: 'The Joker is a rare piece that enables the next move taken to be anywhere on the board. The Joker is normally awarded during crafting when all of the pieces on the board has a lost a certain amount of heat.', image: '/images/Joker.png'}
+		],
+		reversePieces: [
+			{ number: 10, name: 'Reverse Rook', link: 'Reverse-Rook', description: 'sample description', image: '/images/Reverse-Rook.png' },
+			{ number: 11, name: 'Reverse Bishop', link: 'Reverse-Bishop', description: 'sample description', image: '/images/Reverse-Bishop.png' },
+			{ number: 12, name: 'Reverse Queen', link: 'Reverse-Queen', description: 'sample description', image: '/images/Reverse-Queen.png' },
+		],
+		superPieces: [
+			{ number: 13, name: 'Super Rook', link: 'Super-Rook', description: 'sample description', image: '/images/Super-Rook.png' },
+			{ number: 14, name: 'Super Bishop', link: 'Super-Bishop', description: 'sample description', image: '/images/Super-Bishop.png' },
+			{ number: 15, name: 'Super Queen', link: 'Super-Queen', description: 'sample description', image: '/images/Super-Queen.png' },
+		]
+	},
 	all: function(){
-		return this.pieces;
+		const allPiece = [...this.pieces.basicPieces, ...this.pieces.reversePieces, ...this.pieces.superPieces];
+		return allPiece;
+	},
+	basic: function() {
+		return this.pieces.basicPieces;
+	},
+	reverse: function() {
+		return this.pieces.reversePieces;
+	},
+	super: function() {
+		return this.pieces.superPieces;
 	},
 	get: function(name) {
-		const isPiece = p => p.link === name
-		return this.pieces.find(isPiece)
+		const isPiece = p => p.link === name;
+		const allPiece = [...this.pieces.basicPieces, ...this.pieces.reversePieces, ...this.pieces.superPieces];
+		return allPiece.find(isPiece)
 	}
 }
 
