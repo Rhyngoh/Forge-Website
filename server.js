77b116f329ccb35782f2cc5a3fe18fb0978055a0
rhyngoh <ngoh.ryan@gmail.com>
2018-09-12 15:02:40 -0500

    added changes to packagejson for building

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import mongoose from 'mongoose';
+import path from 'path';
 import { getSecret } from './secrets';
 import Boards from './model/BoardSchema';
 
 const app = express();
 const router = express.Router();
 
-const port = process.env.API_PORT || 3001;
+const port = process.env.PORT || 3001;
 
 mongoose.connect(getSecret('dbUri')).then(
 	() => {console.log('Database is connected')}, 
@@ -19,25 +20,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + '/build')));
 
-app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Credentials', 'true');
-	res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+// app.use(function(req, res, next) {
+// 	res.setHeader('Access-Control-Allow-Origin', '*');
+// 	res.setHeader('Access-Control-Allow-Credentials', 'true');
+// 	res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+// 	res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 
-	res.setHeader('Cache-Control', 'no-cache');
-	next();
-});
+// 	res.setHeader('Cache-Control', 'no-cache');
+// 	next();
+// });
 
 app.get('/', (req, res) => {
 	res.json({ message: 'API initialized!'});
-});
-
-app.get('/api/getList', (req, res) => {
-	var list = ["item1", "item2", "item3"];
-	res.json(list);
 });
 
 app.get('/boards', (req, res) => {
@@ -71,7 +67,7 @@ app.post('/boards', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname+'index.html'));
+	res.sendFile(path.join(__dirname+'/build/index.html'));
 });
 
 app.listen(port, function() {
